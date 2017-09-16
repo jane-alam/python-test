4468353ae16cf2008dbdd2c29b9074e319294143
Mahammad Jan-E-Alam <alam@Mahammads-MacBook.local>
2017-09-16 21:02:18 +0600

    as

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import os
 from app import create_app
 
-app = create_app('blueprints')
+app = create_app()
 
 if __name__ == '__main__':
-   app.run(debug=True, use_reloader=True)
+   port = int(os.environ.get("PORT", 5000))
+   app.run(debug=True, port=port, use_reloader=True)
