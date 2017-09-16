a06c179a76202e0c67ac757384bde044aa8b7134
Jane Alam <janealam416@gmail.com>
2017-09-17 02:40:51 +0600

    Testing py file

--- views.py
+++ views.py
@@ -1,6 +1,13 @@
 from app import app
 from models import Users
+from forms import LoginForm
+from flask import render_template
 
 @app.route('/')
 def homepage():
     return "Hello World"
+
+@app.route('/login')
+def Login():
+    form = LoginForm()
+    return render_template('login.html', form = form)
